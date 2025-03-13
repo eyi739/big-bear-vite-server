234d85894efca31fc97039346ed8c901e7e16925
eyi739 <168599229+eyi739@users.noreply.github.com>
2025-03-12 21:17:12 -0700

    a commit.

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 app.get("/api", (req,res) => {
-    res.json({fruits: ["apple", "orange", "banana", "green grapes", "guavasss" ]});
+    res.json({fruits: ["apple", "orange", "banana", "green grapes", "guavas" ]});
 })
 
 app.listen(8080, () => {
