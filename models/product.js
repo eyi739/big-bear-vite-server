468da4696a54bc9dfef7ccec6d64fb0a9bbfa212
eyi739 <168599229+eyi739@users.noreply.github.com>
2025-03-20 23:55:01 -0700

    added product and review mongoose models.

---
+++ models/product.js
@@ -0,0 +1,77 @@
+const mongoose = require('mongoose');
+const Review = require('./review')
+const Schema = mongoose.Schema;
+
+const ImageSchema = new Schema ({
+    url: String,
+    filename: String
+})
+
+ImageSchema.virtual('thumbnail').get(function() {
+    return this.url.replace('/upload', '/upload/w_200')
+})
+
+const opts = { toJSON: {virtuals: true}}
+
+const productSchema = new Schema ({
+    name: {
+        type: String,
+        required: true
+    },
+    images: [ImageSchema],
+    geometry: {
+        type: {
+            type: String,
+            enum: ['Point'],
+            required: true
+        },
+        coordinates: {
+            type: [Number],
+            required: true
+        }
+    },
+    price: {
+        type: Number,
+        required: true,
+        min: 0
+    },
+    category: {
+        type: String,
+        lowercase: true,
+        enum: ['fruit', 'vegetable', 'dairy', 'meat', 'poultry']
+    },
+    description: String,
+    location: String,
+    reviews: [
+        {   
+            type: Schema.Types.ObjectId,
+            ref: 'Review'
+        }
+    ],
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }
+}, opts);
+
+productSchema.virtual('properties.popUpMarkup').get(function(){
+    return `
+    <strong><a href="/products/${this._id}">${this.name}</a></strong>
+    <p>${this.description.substring(0 , 20)}...</p>`
+})
+
+// productSchema.virtual('properties.popUpMarkup').get(function () {
+//     return `
+//     <strong><a href="/products/${this._id}">${this.name}</a><strong>
+//     <p>${this.description.substring(0, 20)}...</p>`
+// });
+
+productSchema.post('findOneAndDelete', async function(doc){
+    await Review.deleteMany({
+        _id: {
+            $in: doc.reviews
+        }
+    })
+})
+
+module.exports = mongoose.model('Product', productSchema);
