5732c22812d224a7a10bfdf49ae00172aa61ad7b
eyi739 <168599229+eyi739@users.noreply.github.com>
2025-03-30 22:18:27 -0700

    routing to React components using react-router-dom.

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require('path');
 const app = express();
 const cors = require("cors");
 const corsOptions = {
@@ -26,22 +27,27 @@ mongoose.connect('mongodb://127.0.0.1:27017/bigBearVite')
 //     console.log("Database connected")
 // })
 
+// app.set('views', path.join(__dirname, 'big-bear-vite', 'src', 'views'));
+
+// C:\Users\yieri\OneDrive\Desktop\Sites\big-bear-vite\src\views\products\ProductIndex.jsx
+
 app.use(cors(corsOptions));
 
-app.get("/products", async (req,res) => {
-    const products = await Product.find({})
-    res.render('produccts/index')
-})
+// app.get('/products', async (req,res) => {
+//     const products = await Product.find({});
+//     res.render('/products/productIndex');
+// })
 
-app.get("/api", (req,res) => {
+
+app.get('/api', (req,res) => {
     res.json({fruits: ["apple", "orange", "banana", "green grapes", "guavas" ]});
 })
 
-app.get('/products', async (req, res) => {
-    const products = await Product.find({});
-    console.log(products);
-    res.send("ALL PRODUCTS WILL BE HERE");
-})
+// app.get('/products', async (req, res) => {
+//     const products = await Product.find({});
+//     console.log(products);
+//     res.send("ALL PRODUCTS WILL BE HERE");
+// })
 
 app.listen(8080, () => {
     console.log("Server started on port 8080");
