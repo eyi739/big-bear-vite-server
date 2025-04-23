36973feca7cbc65aa5082f6dccdbb930eab6848a
eyi739 <168599229+eyi739@users.noreply.github.com>
2025-04-22 17:46:05 -0700

    configuring seeds and routes.

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Main from "../../../big-bear-vite/src/layouts/Main/Main.jsx";
+import Main from "../../../big-bear-vite/src/layouts/Main.jsx";
 import HomePage from '../../../big-bear-vite/src/pages/Home/HomePage.jsx';
 
 const router = createBrowserRouter([
--- src/seeds/index.js
+++ src/seeds/index.js
@@ -19,8 +19,8 @@ const seedDB = async () => {
         const randomNum = Math.floor(Math.random() * 5)
         const prod = new Product({
             name: `${products[randomNum].name}`,
+            price: `${products[randomNum].price}`,
             category: `${products[randomNum].category}`,
-            price: `${products[randomNum].price}`
         })
         await prod.save();
     }
