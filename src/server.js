c43e79aa5f75101c2793c22ddf3f977a490c6504
eyi739 <168599229+eyi739@users.noreply.github.com>
2025-04-29 19:55:52 -0700

    connecting express to React application.

--- src/server.js
+++ src/server.js
@@ -1,44 +1,13 @@
 import express from 'express';
-import webpack from 'webpack';
-import webpackDevMiddleware from 'webpack-dev-middleware';
-import webpackConfig from '../webpack.config.cjs';
-import webpackHotMiddleware from 'webpack-hot-middleware';
 
 import Product from './models/product.js'
 import mongoose from 'mongoose';
 
 import path from 'path';
 import cors from 'cors';
-
 const corsOptions = {
     origin: ["http://localhost:5173"]
 }
-
-const HOST = process.env.SERVER_HOST;
-const PORT = process.env.SERVER_PORT;
-
-const app = express();
-const port = 8080;
-
-// Logging Middleware 
-app.use((req, res, next) => {
-    console.log('-----------------------');
-    console.log(`Request: ${req.method} ${req.url}`);
-    return next();
-});
-
-const compiler = webpack(webpackConfig);
-app.use(webpackDevMiddleware(compiler, {
-    publicPath: webpackConfig.output.publicPath
-}));
-app.use(webpackHotMiddleware(compiler, {}));
-
-
-app.use(cors(corsOptions));
-// const Product = require('../models/product');
-// const mongoose = require("mongoose");
-// const { webpack } = require("webpack");
-
 
 mongoose.connect('mongodb://127.0.0.1:27017/bigBearVite')
     .then(() => {
@@ -49,32 +18,47 @@ mongoose.connect('mongodb://127.0.0.1:27017/bigBearVite')
         console.log(err);
     });
 
+const HOST = process.env.VITE_SERVER_HOST;
+const PORT = process.env.VITE_SERVER_PORT;
 
+const app = express();
+const port = 8080;
+
+app.use(cors(corsOptions));
+// Logging Middleware 
+app.use((req, res, next) => {
+    console.log('-----------------------');
+    console.log(`Request: ${req.method} ${req.url}`);
+    return next();
+});
 // we can alternatively use db.on and db.once for our error handling
 // const db = mongoose.connection;
 // db.on("error", console.error.bind(console, "connection error:"))
 // db.once("open", () => {
 //     console.log("Database connected")
 // })
+const ApiRouter = express.Router();
 
-// app.set('views', path.join(__dirname, 'views'))
-
-const router = express.Router();
-
-
-router.get('/api/home', (req,res) => {
-     res.json({message: 'HELLO FROM EXPRESS. THIS WILL BE THE HOME PAGE'});
+ApiRouter.get('/home', (req,res) => {
+     return res.json({message: 'HELLO FROM EXPRESS. THIS WILL BE THE HOME PAGE'});
+});
+ApiRouter.get('/about', (req,res) => {
+     return res.json({message: 'HELLO FROM EXPRESS. THIS WILL BE THE ABOUT PAGE'});
+});
+ApiRouter.get('/contact', (req,res) => {
+     return res.json({message: 'HELLO FROM EXPRESS. THIS WILL BE THE CONTACT PAGE'});
 });
 
-// app.get('/api/home', (req,res) => {
-//      res.json({message: 'HELLO FROM EXPRESS. THIS WILL BE THE HOME PAGE'});
-// });
+app.get('/home', (req,res) => {
+     return res.json({message: 'HELLO FROM EXPRESS. THIS IS HOMEPAGE DATA'});
+});
 
 app.get('/api', (req,res) => {
-    res.json({fruits: ["apple", "orange", "banana", "green grapes", "tomatoes" ]});
+    return res.json({fruits: ["apple", "orange", "banana", "green grapes", "tomatoes" ]});
 });
 
+app.use('/api', ApiRouter);
 
-app.listen(port, () => {
-    console.log(`Server started on port ${port}`);
+app.listen(PORT, HOST, () => {
+    console.log(`Server started on port ${HOST}: ${PORT}`);
 });
