a61df4a7c0006d365a3591b11ab4bce680b8551d
eyi739 <168599229+eyi739@users.noreply.github.com>
2025-05-20 21:09:39 -0700

    beginning to implement ssr.

---
+++ src/entry-server.jsx
@@ -0,0 +1,14 @@
+// // Library imports
+// import React from 'react';
+// import ReactDOMServer from 'react-dom/server';
+// import { StaticRouter } from 'react-router-dom/server';
+
+// // App level imports 
+// // import local Router file here
+// import { Router } from './route'
+// export function render(){
+//     const html = ReactDOMServer.renderToString(
+//         <React.StrictMode>{/*Static Router goes here*/}</React.StrictMode>
+//     )
+//     return {html}
+// }
